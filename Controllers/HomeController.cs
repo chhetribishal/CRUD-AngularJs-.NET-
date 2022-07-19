043e80e5e51ab9e9845602b5f3ca9861a463be68
chhetribishal <bishal.in.thuma012@gmail.com>
2022-07-19 19:22:47 +0545

    "AngularJsProduct"

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using CRUDoperationAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Core.Objects;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDoperationAPP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        //Insert 
+        [HttpPost]
+        public string InsertProductRecord(tbl_ProductMaster pd)
+        {
+            
+            using (ProductDBEntities db = new ProductDBEntities())
+            {
+                db.tbl_ProductMaster.Add(pd);
+                db.SaveChanges();
+                return "Student Added Successfully!";
+            }
+        }
+
+
+        //Retrieve
+        public JsonResult GetAllProduct()
+        {
+            ProductDBEntities db = new ProductDBEntities();
+            var AllRecord = db.sp_GetAllProducts();
+            return Json(AllRecord, JsonRequestBehavior.AllowGet);
+        }
+
+
+        //Update
+        [HttpPost]
+        public string UpdateProductRecord(tbl_ProductMaster pd)
+        {
+            
+
+            using (ProductDBEntities db = new ProductDBEntities())
+            {
+                var record = db.tbl_ProductMaster.Where(x => x.ProductID == pd.ProductID).FirstOrDefault();
+                record.ProductName = pd.ProductName;
+                record.Price = pd.Price;
+                record.Qty = pd.Qty;
+                record.Remarks = pd.Remarks;
+                
+                
+                db.SaveChanges();
+                return "Product Updated Successfully!";
+            }
+        }
+
+
+        //Delete
+        [HttpPost]
+        public string DeleteProduct(tbl_ProductMaster pd)
+        {
+            using (ProductDBEntities db = new ProductDBEntities())
+            {
+                var data = db.tbl_ProductMaster.Where(x => x.ProductID == pd.ProductID).FirstOrDefault();
+                 db.tbl_ProductMaster.Remove(data);
+                db.SaveChanges();
+                return "Record Deleted Successfully";
+            }
+        }
+    }
+}
---
+++ Scripts/Products.js
@@ -0,0 +1,85 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCntrl", function ($scope, $http) {
+    $scope.GetAllProduct = function () {
+        $http({
+            method: "get",
+            url: "http://localhost:50082/Home/GetAllProduct"
+        }).then(function (response) {
+            $scope.Product = response.data
+        }, function () {
+            alert("Error Occour!");
+        }
+        )
+    };
+
+    $scope.InsertProduct = function () {
+     
+        var type = document.getElementById("insertProd").getAttribute("value");
+        if (type == "Submit") { 
+            $scope.Product = {};
+            $scope.Product.ProductName = $scope.pname;
+            $scope.Product.Price = $scope.price;
+            $scope.Product.Qty = $scope.qty;
+            $scope.Product.Remarks = $scope.remarks;
+            $http({
+                method: "post",
+                url: "http://localhost:50082/Home/InsertProductRecord",
+                datatype: "json",
+                data: JSON.stringify($scope.Product)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllProduct()
+                $scope.pname = " ";
+                $scope.price = " ";
+                $scope.qty = " ";
+                $scope.remarks = " ";
+            })
+        } else {
+
+            $scope.Product = {};
+            $scope.Product.ProductName = $scope.pname;
+            $scope.Product.ProductID = sessionStorage.getItem("PID");
+            $scope.Product.Price = $scope.price;
+            $scope.Product.Qty = $scope.qty;
+            $scope.Product.Remarks = $scope.remarks;
+            $http({
+                method: "post",
+                url: "http://localhost:50082/Home/UpdateProductRecord",
+                datatype: "json",
+                data: JSON.stringify($scope.Product)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllProduct();
+                $scope.pname = " ";
+                $scope.price = " ";
+                $scope.qty = " ";
+                $scope.remarks = " ";
+                document.getElementById("insertProd").setAttribute("value", "Submit");
+            })
+        }
+    };
+
+    $scope.UpdateProduct = function (pd) {
+       
+        sessionStorage.setItem("PID", pd.ProductID);
+        $scope.pname = pd.ProductName;
+        $scope.price = pd.Price;
+        $scope.qty = pd.Qty;
+        $scope.remarks = pd.Remarks;
+        document.getElementById("insertProd").setAttribute("value", "Update");
+    };
+
+ 
+    $scope.DeleteProduct = function (pd) {
+       
+        $http({
+            method: "post",
+            url: "http://localhost:50082/Home/DeleteProduct",
+            datatype: "json",
+            data: JSON.stringify(pd)
+        }).then(function (response) {
+            alert("Deleted Successfully");
+            $scope.GetAllProduct();
+        })
+    };
+});
